ef2f9e1f74300f555414382cd044b14c093bbff8
Ali Noori <ali.noori.dev@gmail.com>
2024-12-17 19:10:30 +0330

    feat: request notification permission on app load

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 import { NetworkStatus, WeatherSearch } from "./components";
 import "./styles/main.scss";
+import { requestNotificationPermission } from "./utils";
 
 function App() {
+  useEffect(() => {
+    requestNotificationPermission();
+  }, []);
+
   return (
     <main className="app-main">
       <WeatherSearch />
