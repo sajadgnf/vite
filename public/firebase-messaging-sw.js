24136d36625603ad3768644f510f9fab314b3e28
ali-noori-dev <ali.noori.dev@gmail.com>
2024-12-18 10:10:34 +0330

    refactor: consolidate Firebase configuration and update Firebase SDK version in service worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,9 +1,17 @@
-import { firebaseConfig } from "../src/config/firebaseConfig";
-
-importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
 importScripts(
-  "https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging.js"
+  "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
 );
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8Pj9hYcNZfPBqEiLJYF8qxfAhOpi3nK0",
+  authDomain: "pwa-weather-tracker.firebaseapp.com",
+  projectId: "pwa-weather-tracker",
+  storageBucket: "pwa-weather-tracker.firebasestorage.app",
+  messagingSenderId: "1001682485557",
+  appId: "1:1001682485557:web:d67bccde71db56fe4c4c7c",
+  measurementId: "G-4QLFF8CKSS",
+};
 
 firebase.initializeApp(firebaseConfig);
 
--- src/config/firebaseInit.ts
+++ src/config/firebaseInit.ts
@@ -1,6 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
-import { firebaseConfig } from "./firebaseConfig.ts";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8Pj9hYcNZfPBqEiLJYF8qxfAhOpi3nK0",
+  authDomain: "pwa-weather-tracker.firebaseapp.com",
+  projectId: "pwa-weather-tracker",
+  storageBucket: "pwa-weather-tracker.firebasestorage.app",
+  messagingSenderId: "1001682485557",
+  appId: "1:1001682485557:web:d67bccde71db56fe4c4c7c",
+  measurementId: "G-4QLFF8CKSS",
+};
 
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
