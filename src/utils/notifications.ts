f8bfc29e8792e99cfa74058f188f6c7f4291a216
ali-noori-dev <ali.noori.dev@gmail.com>
2024-12-18 10:10:49 +0330

    feat: enhance requestNotificationPermission to register service worker for FCM token retrieval

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -5,8 +5,13 @@ const requestNotificationPermission = async () => {
   try {
     const permission = await Notification.requestPermission();
     if (permission === "granted") {
+      const registration = await navigator.serviceWorker.register(
+        "/firebase-messaging-sw.js"
+      );
+
       const token = await getToken(messaging, {
         vapidKey: import.meta.env.VITE_FIREBASE_WEB_PUSH_CERTIFICATE_KEY_PAIR,
+        serviceWorkerRegistration: registration,
       });
       console.log("FCM Token:", token);
       // Send the token to your server to save it for sending notifications
