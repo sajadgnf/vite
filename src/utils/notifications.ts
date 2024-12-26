1e054fa32ca2861f2d399cb31ffb2ff0c2216a9f
Ali Noori <ali.noori.dev@gmail.com>
2024-12-26 12:53:57 +0330

    refactor: remove Firebase configuration and related files

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -1,20 +1,7 @@
-import { getToken } from "firebase/messaging";
-import { messaging } from "../config/firebaseInit.ts";
-
 const requestNotificationPermission = async () => {
   try {
     const permission = await Notification.requestPermission();
     if (permission === "granted") {
-      const registration = await navigator.serviceWorker.register(
-        "/firebase-messaging-sw.js"
-      );
-
-      const token = await getToken(messaging, {
-        vapidKey: import.meta.env.VITE_FIREBASE_WEB_PUSH_CERTIFICATE_KEY_PAIR,
-        serviceWorkerRegistration: registration,
-      });
-      console.log("FCM Token:", token);
-      // Send the token to your server to save it for sending notifications
     } else {
       console.error("Permission not granted for notifications");
     }
