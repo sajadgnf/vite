26a4e2e59aa95c07707a82f0dc723ec6fef33959
ali-noori-dev <ali.noori.dev@gmail.com>
2024-12-31 02:02:10 +0330

    refactor: remove notification permission request logic and related utility

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { useEffect } from "react";
 import { NetworkStatus, WeatherSearch } from "./components";
 import "./styles/main.scss";
-import { requestNotificationPermission } from "./utils";
 
 function App() {
-  useEffect(() => {
-    requestNotificationPermission();
-  }, []);
-
   return (
     <main className="app-main">
       <WeatherSearch />
